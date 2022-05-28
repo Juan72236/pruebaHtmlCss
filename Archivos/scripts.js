1aafd4bd256770eaedb50dbfb6858aedcafeb72c
Juan Pablo <juanpablov018@gmail.com>
2022-05-28 13:43:32 -0500

    version 2

---
+++ Archivos/scripts.js
@@ -0,0 +1,6 @@
+var boton = document.getElementById('registrar');
+function mensaje(){
+    texto=Window.alert('registro exitoso');
+    return(texto);
+}
+btn.addEventListener('click',mensaje,true);
